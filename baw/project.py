9ef459b8b9234cc3c0ca4cc1e661f899bfdbe36a
马振华 <mazhenhua2002035@163.com>
2024-05-25 22:46:28 +0800

    增加云盘测试用例

--- baw/project.py
+++ baw/project.py
@@ -5,3 +5,8 @@ def listAllProjectByCondition(url,headers, br,cs):  # 获取用户列表
     url = url + "/newProject/listProjectByCondition"
     r = br.post_hearder(url,headers,json=cs)
     return r
+
+def getActivityLogByCreateId(url,headers, br,cs):  # 获取用户列表
+    url = url + "/newProject/getActivityLogByCreateId"
+    r = br.post_hearder(url,headers,json=cs)
+    return r
--- scripts/test_document.py
+++ scripts/test_document.py
@@ -1,29 +1,30 @@
 import  pytest
 
 # µÇÂ¼µÄÊý¾Ý
-from baw import project
+from baw import document
 from baw import member
 from caw import fileRead
 
-@pytest.fixture(params=fileRead.read_yaml("/data_case/Project.yaml"))
+@pytest.fixture(params=fileRead.read_yaml("/data_case/document.yaml"))
 def login_data(request):
     return request.param
 
 
-def test_project(login_data, url, br, db_info):
+def test_doc_1(login_data, url, br, db_info):
     r = member.login(url, br, login_data['logindata'])
     headers = {'Lang': "CN",
                'Authorization': r.json()["data"]["jwtToken"],
                'User-Company': r.json()["data"]["companyId"]}
-    r1=project.listAllProjectByCondition(url,headers, br,login_data['listAllProjectByCondition'])
+    r1=document.listAppDocument(url,headers, br,login_data['listAppDocument'])
     assert str(r1.json()['message']) == str(login_data['exp']['message'])
-    assert str(r1.json()["data"]["total"]) == str(login_data['exp']['datatotal'])
+    assert str(r1.json()['data'][0]["attachmentName"]) == str(login_data['attachmentName']['attachmentName1'])
 
-def test_project(login_data, url, br, db_info):
+def test_doc_2(login_data, url, br, db_info):
     r = member.login(url, br, login_data['logindata'])
     headers = {'Lang': "CN",
                'Authorization': r.json()["data"]["jwtToken"],
                'User-Company': r.json()["data"]["companyId"]}
-    r1=project.getActivityLogByCreateId(url,headers, br,login_data['getActivityLogByCreateId'])
+    r1=document.listShareDocument(url,headers, br,login_data['listShareDocument'])
     assert str(r1.json()['message']) == str(login_data['exp']['message'])
-    assert str(r1.json()["data"]["totalNumber"]) >= str(login_data['exp']['getActivityLog'])
+    assert str(r1.json()['data'][0]["attachmentName"]) == str(login_data['attachmentName']['attachmentName2'])
+
