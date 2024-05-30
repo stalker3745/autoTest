6861910a6a52d73051c276231767c13912c05ca9
马振华 <systeminformation@zdsztech.com>
2024-05-30 20:10:02 +0800

    表单查询用例

--- scripts/test_from.py
+++ scripts/test_from.py
@@ -15,20 +15,25 @@ def test_from_1(form_data, url, br,db_info):
     headers = {'Lang': "CN",
                'Authorization': r.json()["data"]["jwtToken"],
                'User-Company': r.json()["data"]["companyId"]}
-
     rx= from1.getAppListByPackage(url,headers, br, form_data['getAppListByPackage'])
-    print(rx.json())
+    assert str(rx.json()['message']) == str(form_data['exp']['message'])
+    assert str(rx.json()['code']) == str(form_data['exp']['code'])
     rx = from1.getAppById(url, headers,br, form_data['getAppById'])
-    print(rx.json())
-
-    viewId=from1.getFormViewConfigList(url,headers, br, form_data['logindata'])
+    assert str(rx.json()['message']) == str(form_data['exp']['message'])
+    assert str(rx.json()['code']) == str(form_data['exp']['code'])
+    form1=from1.getFormViewConfigList(url,headers, br, form_data['getFormViewConfigList'])
+    assert str(form1.json()['message']) == str(form_data['exp']['message'])
+    assert str(form1.json()['code']) == str(form_data['exp']['code'])
+    viewId=form1.json()["data"][0]["viewId"]
+    viewIden=form1.json()["data"][0]["viewType"]
     getFromFieldEnumList = {'language': "CN",
                    'companyId': r.json()["data"]["companyId"],
-                    "viewId":"1217619369606336512",
-                    "viewIden": 1,
+                    "viewId":viewId,
+                    "viewIden": viewIden,
                     "fieldId": "fd_1676955337149member",
-                    "appId": "1075773133891485696"
+                    "appId": form_data['getAppById']["appId"]
                             }
     r1=from1.getFromFieldEnumList(url,headers, br,getFromFieldEnumList)
     assert str(r1.json()['message']) == str(form_data['exp']['message'])
-    print(r1.json())
+    assert str(r1.json()['code']) == str(form_data['exp']['code'])
+    assert len(r1.json()["data"]) == form_data['exp']['int1']
