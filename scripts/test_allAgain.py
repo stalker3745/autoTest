00b39ab8c11518e309978b2aea887b9e414cba9b
马振华 <systeminformation@zdsztech.com>
2024-06-05 10:18:31 +0800

    排查utf-8

--- scripts/test_allAgain.py
+++ scripts/test_allAgain.py
@@ -6,7 +6,7 @@ from caw import fileRead
 
 
 class TestAll:
-    @pytest.fixture(params=fileRead.read_yaml("/data_case/all.yaml"))
+    @pytest.fixture(params=fileRead.read_yaml("/data_case/all1.yaml"))
     def project_Resources(request):
         return request.param
 
@@ -22,20 +22,16 @@ class TestAll:
     def test_findAll_project(self,url,br,getHeaders,project_Resources):
         # 这是个现成的接口，查看用户列表用的
         r=project.listAllProjectByCondition(url,br,getHeaders,project_Resources['listAllProjectByCondition'])
-
         # 使用 JSONPath 提取 JSON 数据中的 message 和 code 字段
         message=jsonpath.parse('$.message')
         code=jsonpath.parse('$.code')
-
         message_match=message.find(r.json())
         code_match=code.find(r.json())
-
         assert str(message_match[0].value)==str(project_Resources['exp']['message'])
         assert str(message_match[0].value)==str(project_Resources['exp']['code'])
 
     def test_findAllUser_project(self,url,br,getHeaders,project_Resources):
         # 这是个现成的接口，查看用户列表用的
         r=project.getActivityLogByCreateId(url,br,getHeaders,project_Resources['getActivityLogByCreateId'])
-
         assert str(r.json()['message'])==str(project_Resources['exp']['message'])
         assert str(r.json()['code'])==str(project_Resources['exp']['code'])
