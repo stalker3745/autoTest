2eb40b0deb8b45e90b09429bd700445c2a214222
马振华 <mazhenhua2002035@163.com>
2024-06-30 18:20:48 +0800

    删除查询项目多余信息，后期需要再次修改。

--- scripts/test_notice.py
+++ scripts/test_notice.py
@@ -5,36 +5,20 @@ import  pytest
 from baw import member
 from caw import fileRead
 
-@pytest.fixture(params=fileRead.read_yaml("/data_case/notice.yaml"))
-def notice_data(request):
-    return request.param
+# @pytest.fixture(params=fileRead.read_yaml("/data_case/notice.yaml"))
+# def notice_data(request):
+#     return request.param
 
 
 def test_notice(notice_data, url, br,getHeaders):
-    # r = member.login(url, br, notice_data['logindata'])
-    # headers = {'Lang': "CN",
-    #            'Authorization': r.json()["data"]["jwtToken"],
-    #            'User-Company': r.json()["data"]["companyId"]}
-    r1=member.notice(url,getHeaders, br,notice_data['pares'])
-    assert str(r1.json()['message']) == str(notice_data['exp']['message'])
-    assert str(r1.json()['code']) == str(notice_data['exp']['code'])
-    # r2=member.newGetMessageList(url,headers, br,notice_data['newGetMessageList'])
-    # assert str(r2.json()['message']) == str(notice_data['exp']['message'])
-    # assert str(r2.json()['data']["total"]) == str(notice_data['expdate']['total'])
+    pares= {"page": 1, "pageSize": 3}
+    r1=member.notice(url,getHeaders, br,pares)
+    assert str(r1.json()['message']) == "操作成功!"
+    assert str(r1.json()['code']) == "200"
 
 def test_newGetMessageList(notice_data, url, br,getHeaders):
-    # r = member.login(url, br, notice_data['logindata'])
-    # headers = {'Lang': "CN",
-    #            'Authorization': r.json()["data"]["jwtToken"],
-    #            'User-Company': r.json()["data"]["companyId"]}
-    # r1=member.notice(url,headers, br,notice_data['pares'])
-    # assert str(r1.json()['message']) == str(notice_data['exp']['message'])
-    # assert str(r1.json()['code']) == str(notice_data['exp']['code'])
     newGetMessageList = {"page": 1, "pageSize": 10, "companyId": getHeaders["User-Company"], "messageGroupId": "5"}
-    print(url)
-    print(getHeaders)
-    print(newGetMessageList)
     r2=member.newGetMessageList(url, getHeaders, br, newGetMessageList)
-    print(r2)
-    assert str(r2.json()['message']) == str(notice_data['exp']['message'])
-    assert str(r2.json()['data']["total"]) == str(notice_data['exp']['total'])
+    assert str(r2.json()['message']) == "操作成功!"
+    assert str(r2.json()['code']) == "200"
+    assert str(r2.json()['data']["total"]) == "10"
